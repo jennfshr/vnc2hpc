41c057b5b2862bfe7f886eda981ecc0fe115dcf4
Jennifer Kathleen Green <jgreen@lanl.gov>
2020-08-09 07:20:20 -0600

    improvements to the backend to start trying to evaluate support for twm and xfwm4- consolidated - implemented the xclients geometry

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -5,12 +5,12 @@ vnc2hpc_version=$2
 windowmanager=$3
 client=$4
 clientOS=$5
-/usr/bin/vncserver :$displayport -name "$USER at `hostname -s`:$displayport $windowmanager" -autokill -depth 16 -xstartup /usr/projects/hpcsoft/common/openbox/3.6.1/etc/xdg/openbox/xstartup &>$LOG &
+/usr/bin/vncserver :$displayport -name "$USER at `hostname -s`:$displayport $windowmanager" -autokill -depth 16 -xstartup /usr/projects/hpcsoft/vnc2hpc/0.0.2/bin/xstartup  &>$LOG &
 pid=$!
 while kill -0 "$pid"&>/dev/null; do
    sleep 1
 done 
 #New 'jgreen at fg-fey1:14 mwm' desktop is fg-fey1.lanl.gov:17
 displayport=$(awk -F: '/^New/ {print $3}' $LOG) 
-echo "USER=$USER HOSTNAME=`hostname -s` PID=$pid WINDOWMANAGER=$windowmanager VNC2HPC_VERSION=$vnc2hpc_version CLIENT=$client CLIENTOS=$clientOS COMMAND:(/usr/bin/vncserver :$displayport -name "$USER at `hostname -s`:$displayport $windowmanager" -autokill -depth 16 -xstartup /usr/projects/hpcsoft/common/openbox/3.6.1/etc/xdg/openbox/xstartup &>/dev/null )" &> $LOG
+echo "USER=$USER HOSTNAME=`hostname -s` PID=$pid WINDOWMANAGER=$windowmanager VNC2HPC_VERSION=$vnc2hpc_version CLIENT=$client CLIENTOS=$clientOS COMMAND:(/usr/bin/vncserver :$displayport -name "$USER at `hostname -s`:$displayport $windowmanager" -autokill -depth 16 -xstartup /usr/projects/hpcsoft/vnc2hpc/0.0.2/bin/xstartup &>/dev/null )" &> $LOG
 echo $displayport
