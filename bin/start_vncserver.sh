d6e61b0a0562e30d5275c74944498146c115928a
Jennifer Kathleen Green <jgreen@lanl.gov>
2020-08-10 17:00:20 -0600

    fixing bug in testing parameter 5

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -3,7 +3,7 @@ vnc2hpc_version="$1"
 windowmanager="$2"
 client="$3"
 clientOS="$4"
-if [[ "$5x" -ne x ]] ; then DISPLAYPORT=":$5" ; fi 
+if [[ "${5}x" != x ]] ; then DISPLAYPORT=":${5}" ; fi 
 LOG="/usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/logs/$(/usr/projects/hpcsoft/utilities/bin/sys_name)/${USER}_`hostname -s`.$(date +%F'_'%H'.'%M'.'%S)"
 touch $LOG
 /usr/bin/vncserver ${DISPLAYPORT} -name "$USER at `hostname -s`:$displayport $windowmanager" -autokill -depth 16 -xstartup "/usr/projects/hpcsoft/vnc2hpc/0.0.2/bin/xstartup $windowmanager" &>$LOG
