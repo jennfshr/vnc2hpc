9653dc0e8b80b1e70ea770427beda2dc315a16d3
Jennifer Kathleen Green <jgreen@lanl.gov>
2020-11-12 13:11:11 -0700

    replaced hardcoded version string to use the vnc2hpc_version variable

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -7,7 +7,7 @@ if [[ "${5}x" != x ]] ; then DISPLAYPORT=":${5}" ; fi
 LOG="/usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/logs/$(/usr/projects/hpcsoft/utilities/bin/sys_name)/${USER}_`hostname -s`.$(date +%F'_'%H'.'%M'.'%S)"
 touch $LOG
 [ -d $HOME/.vnc2hpc ] || mkdir -p $HOME/.vnc2hpc
-cp /usr/projects/hpcsoft/vnc2hpc/0.0.2/bin/xstartup $HOME/.vnc2hpc/xstartup
+cp /usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/bin/xstartup $HOME/.vnc2hpc/xstartup
 export VNC2HPC_WM="$2" 
 /usr/bin/vncserver ${DISPLAYPORT} -localhost -verbose -name "$USER at `hostname -s` VNC2HPC v$vnc2hpc_version $windowmanager `date`" -autokill -depth 16 -xstartup "$HOME/.vnc2hpc/xstartup" &>$LOG
 
