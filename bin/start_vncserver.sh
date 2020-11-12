32e191af987c635afd737d0c6ed43da14183ff78
Jennifer Kathleen Green <jgreen@lanl.gov>
2020-11-12 14:04:49 -0700

    added in geometry to the vncserver call

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -3,13 +3,16 @@ vnc2hpc_version="$1"
 windowmanager="$2"
 client="$3"
 clientOS="$4"
-if [[ "${5}x" != x ]] ; then DISPLAYPORT=":${5}" ; fi 
+geometry="$5"
+if [[ "${6}x" != x ]] ; then DISPLAYPORT=":${6}" ; fi 
 LOG="/usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/logs/$(/usr/projects/hpcsoft/utilities/bin/sys_name)/${USER}_`hostname -s`.$(date +%F'_'%H'.'%M'.'%S)"
 touch $LOG
 [ -d $HOME/.vnc2hpc ] || mkdir -p $HOME/.vnc2hpc
 cp /usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/bin/xstartup $HOME/.vnc2hpc/xstartup
-export VNC2HPC_WM="$2" 
-/usr/bin/vncserver ${DISPLAYPORT} -localhost -verbose -name "$USER at `hostname -s` VNC2HPC v$vnc2hpc_version $windowmanager `date`" -autokill -depth 16 -xstartup "$HOME/.vnc2hpc/xstartup" &>$LOG
+export VNC2HPC_WM="$windowmanager" 
+export GEOMETRY="$geometry"
+if [[ $GEOMETRY != "false" ]] ; then geoarg="-geometry ${geometry}" ; fi 
+/usr/bin/vncserver ${DISPLAYPORT} ${geoarg} -localhost -verbose -name "$USER at `hostname -s` VNC2HPC v$vnc2hpc_version $windowmanager `date`" -autokill -depth 16 -xstartup "$HOME/.vnc2hpc/xstartup" &>$LOG
 
 if [[ $? -ne 0 ]] ; then 
    displayport=FAILURE
