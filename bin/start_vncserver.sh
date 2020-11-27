493e77e7a7f611bae02ef5536b17cc67a2da1d6c
Jennifer Green <jgreen@lanl.gov>
2020-11-27 05:36:10 -0700

    improve logging on the server side

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -6,6 +6,8 @@ clientOS="$4"
 geometry="$5"
 pixeldepth="$6"
 backstore="-bs"
+HPCSOFT_PATH="/usr/projects/hpcsoft"
+SPLUNK_LOG="${HPCSOFT_PATH}/usage_logs/vnc2hpc.log"
 if [[ "${7}x" != x ]] ; then DISPLAYPORT=":${7}" ; fi 
 [ -d $HOME/.vnc2hpc ] || mkdir -p $HOME/.vnc2hpc
 if [[ -d "/usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/logs/$(/usr/projects/hpcsoft/utilities/bin/sys_name)" ]] ; then 
@@ -24,7 +26,10 @@ echo "RUNNING: /usr/bin/vncserver ${DISPLAYPORT} ${backstore} ${geoarg} ${pixeld
 
 if [[ $? -ne 0 ]] ; then 
    displayport=FAILURE
+   RESULT="FAIL"
 else  
    displayport=$(awk -F: '/^New/ {print $NF}' $LOG) 
+   RESULT="PASS"
 fi 
 echo $displayport
+echo "$(date +%F' '%H':'%M':'$S) VNC2HPC_VERSION=${vnc2hpc_version} USER=${USER} CLIENT=${CLIENT} CLIENTOS=${CLIENTOS} $MACHINE=$(hostname -s) WINDOWMANAGER=${windowmanager} VNCSERVER=$(which vncserver) DISPLAYPORT=${displayport} BACKSTORE=${backstore} GEOMETRY=${geoarg} PIXELDEPTH=${pixeldepth} RESULT=${RESULT}" &>>$SPLUNK_LOG
