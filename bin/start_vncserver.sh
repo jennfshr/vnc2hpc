17e438fcc80423a54a6dd8ab4169501cdc373ed2
Jennifer Green <jgreen@lanl.gov>
2020-12-08 13:03:42 -0700

    some comments and a bit of refactoring to start_vncserver.sh

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# process positional parameters
 vnc2hpc_version="$1"
 windowmanager="$2"
 client="$3"
@@ -6,42 +7,68 @@ clientOS="$4"
 geometry="$5"
 pixeldepth="$6"
 remote_install_path="$7"
-export VNC2HPC_INSTALL_PATH=${remote_install_path}
+
+# some conditionals
 if [[ "${8}x" != x ]] ; then DISPLAYPORT=":${8}" ; fi 
+if [[ $geometry != "default" ]] ; then geoarg="-geometry ${geometry}" ; fi 
+
+# setup pixeldepth arg
+pixeldeptharg="-depth ${pixeldepth}" 
+
+# this is required for a weird xvnccrash with MCNP
 backstore="-bs"
+
+export VNC2HPC_INSTALL_PATH=${remote_install_path}
+export VNC2HPC_WM="$windowmanager" 
 HPCSOFT_PATH="/usr/projects/hpcsoft"
 SPLUNK_LOG="${HPCSOFT_PATH}/usage_logs/vnc2hpc.log"
-[ -d $HOME/.vnc2hpc ] || mkdir -p $HOME/.vnc2hpc
-if [[ -d "/usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/logs/$(/usr/projects/hpcsoft/utilities/bin/sys_name)" ]] ; then 
-   LOG="/usr/projects/hpcsoft/vnc2hpc/${vnc2hpc_version}/logs/$(/usr/projects/hpcsoft/utilities/bin/sys_name)/${USER}_`hostname -s`.$(date +%F'_'%H'.'%M'.'%S)"
-else
-   LOG="${HOME}/.vnc2hpc/${USER}_`hostname -s`.$(date +%F'_'%H'.'%M'.'%S)"
-fi 
-cp -f ${remote_install_path}/bin/xstartup $HOME/.vnc2hpc/xstartup || echo "FAILURE"
-touch $LOG
-export VNC2HPC_WM="$windowmanager" 
-if [[ -x /usr/projects/hpcsoft/utilities/bin/sys_os ]] ; then
-   OS=$(/usr/projects/hpcsoft/utilities/bin/sys_os)
+
+# generic setup if utilities aren't present
+if [[ -x ${HPCSOFT_PATH}/utilities/bin/sys_os ]] ; then
+   OS=$(${HPCSOFT_PATH}/utilities/bin/sys_os)
 else
    OS=$(uname -o | sed 's/\//_/g')
 fi
-if [[ -x /usr/projects/hpcsoft/utilities/bin/sys_arch ]] ; then
-   ARCH=$(/usr/projects/hpcsoft/utilities/bin/sys_arch)
+
+# generic setup if utilities aren't present
+if [[ -x ${HPCSOFT_PATH}/utilities/bin/sys_arch ]] ; then
+   ARCH=$(${HPCSOFT_PATH}/utilities/bin/sys_arch)
 else
    ARCH=$(uname -p)
 fi
+
+# generic setup if utilities aren't present
+if [[ -x ${HPCSOFT_PATH}/utilities/bin/sys_name ]] ; then
+   SYSNAME=$(${HPCSOFT_PATH}/utilities/bin/sys_name)
+else
+   SYSNAME=$(hostname -s)
+fi
+
+[ -d $HOME/.vnc2hpc ] || mkdir -p $HOME/.vnc2hpc
+
+# setup log to capture stdout
+if [[ -d "${HPCSOFT_PATH}/vnc2hpc/${vnc2hpc_version}/logs/${SYSNAME}" ]] ; then 
+   LOG="${HPCSOFT_PATH}/vnc2hpc/${vnc2hpc_version}/logs/${SYSNAME}/${USER}_`hostname -s`.$(date +%F'_'%H'.'%M'.'%S)"
+else
+   LOG="${HOME}/.vnc2hpc/${USER}_`hostname -s`.$(date +%F'_'%H'.'%M'.'%S)"
+fi 
+touch $LOG
+
+cp -f ${remote_install_path}/bin/xstartup $HOME/.vnc2hpc/xstartup || echo "FAILURE"
+
 case ${VNC2HPC_WM} in
    berry*)	WM="berry"	;;
    ice*)	WM="icewm"	;;
    fvwm)	WM="fvwm"	;;
    openbox)	WM="openbox"	;;
 esac
+
 if ! [[ -d /usr/projects/hpcsoft/${OS}/${ARCH}/${VNC2HPC_WM} ]] ; then
    ${VNC2HPC_INSTALL_PATH}/libexec/build_wms.sh -w $WM -p ${HOME}/.vnc2hpc &>>$LOG
 fi
-if [[ $geometry != "default" ]] ; then geoarg="-geometry ${geometry}" ; fi 
-pixeldeptharg="-depth ${pixeldepth}" 
-echo "RUNNING: /usr/bin/vncserver ${DISPLAYPORT} ${backstore} ${geoarg} ${pixeldeptharg} -localhost -verbose -name \"$USER at `hostname -s` VNC2HPC v$vnc2hpc_version $windowmanager `date`\" -autokill ${pixeldeptharg} -xstartup \"$HOME/.vnc2hpc/xstartup\"" &>$LOG
+
+echo "RUNNING: /usr/bin/vncserver ${DISPLAYPORT} ${backstore} ${geoarg} ${pixeldeptharg} -localhost -verbose -name \
+               \"$USER at `hostname -s` VNC2HPC v$vnc2hpc_version $windowmanager `date`\" -autokill ${pixeldeptharg} -xstartup \"$HOME/.vnc2hpc/xstartup\"" &>$LOG
 /usr/bin/vncserver ${DISPLAYPORT} ${backstore} ${geoarg} ${pixeldeptharg} -localhost -verbose -name "$USER at `hostname -s` VNC2HPC v$vnc2hpc_version $windowmanager `date`" -autokill ${pixeldeptharg} -xstartup "$HOME/.vnc2hpc/xstartup" &>>$LOG
 
 if [[ $? -ne 0 ]] ; then 
