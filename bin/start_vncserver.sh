370ab37e80fd513faa1736d295119d34650e56e2
Jennifer Green <jgreen@lanl.gov>
2020-12-09 13:51:38 -0700

    make the error detection in start_vncserver work for build failure

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -72,7 +72,7 @@ esac
 if ! [[ -d "/usr/projects/hpcsoft/${OS}/common/${ARCH}/${VNC2HPC_WM}" ]] ; then
    ${VNC2HPC_INSTALL_PATH}/libexec/build_wms.sh -w $WM -p ${HOME}/.vnc2hpc &>>$LOG
    if [ $? -ne 0 ] ; then
-      echo "Build of $WM failed on $(hostname)"
+      echo "Build of $WM FAILURE on $(hostname)"
       exit 1
    fi
 fi
