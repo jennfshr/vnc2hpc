f3df0236e18180368a35c070e8136ab6d1a297ef
Jennifer Green <jgreen@lanl.gov>
2020-12-24 06:24:42 -0700

    making debug output a feature

--- bin/start_vncserver.sh
+++ bin/start_vncserver.sh
@@ -73,10 +73,11 @@ esac
 if [[ ! -d "/usr/projects/hpcsoft/${OS}/common/${ARCH}/${VNC2HPC_WM}" && \
    ! -d "$HOME/.vnc2hpc/${OS}/common/${ARCH}/${VNC2HPC_WM}" && \
    ! $(which $VNC2HPC_WM &>/dev/null) ]] ; then
-   echo "IN BUILD STEP" &> $HOME/debugstartvncserver.out
+   echo "No ${VNC2HPC_WM} found on ${SYSNAME}, building to ${HOME}/.vnc2hpc/${OS}/common/${ARCH}/${VNC2HPC_WM}" &>>$LOG
    ${VNC2HPC_INSTALL_PATH}/libexec/build_wms.sh -w $WM -p ${HOME}/.vnc2hpc &>>$LOG
    if [[ $? -ne 0 ]] ; then
       echo "Build of $WM FAILURE on $(hostname)"
+      cat /tmp/vnc2hpc_deps_${USER}/${VNC2HPC_WM}*/build.log &>>$LOG
       exit 1
    fi
 fi
